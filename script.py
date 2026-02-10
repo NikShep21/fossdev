9d0350cf5d753ea68a304e8ae255f4a2eb440488
NikShep21 <klimenko.leva.lol2@yandex.ru>
2026-02-10 14:33:50 +0800

    added test for string devision and exception in devision

--- script.py
+++ script.py
@@ -2,6 +2,9 @@ def sum(a, b):
     return a + b
 
 def devide(a, b):
+    if isinstance(a, str) or isinstance(b, str):
+        raise ValueError("Строки делить нельзя")
+
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
--- test.py
+++ test.py
@@ -13,7 +13,17 @@ def test_devide():
     result = 2
     assert devide(a, b) == result
 
+def test_deivision_prohibited():
+    try:
+        devide("A", "B")
+        print("Test string division failed")
+        assert False
+    except ValueError as e:
+        print("Test string division passed")
+
 if __name__ == "__main__":
     test_sum()
     test_devide()
+    test_deivision_prohibited()
     print("All tests passed")
+
