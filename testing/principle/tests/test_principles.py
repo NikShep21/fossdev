897b389d68579157a2fd37fbb6b7285f8f8d6715
NikShep21 <klimenko.leva.lol2@yandex.ru>
2026-03-05 13:05:09 +0800

    add demo for do-not-duplicate

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -11,8 +11,12 @@ def test_addition_with_bug():
     assert add_with_bug(2, 2) == 4
     assert add_with_bug(3, 3) == 6
     print("Addition with bug test passed.")
-    
+
+def test_addition_duplicate():
+    assert add(6, 7) == 6 + 7
+    print("Addition duplicate test passed.")
 
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
+    test_addition_duplicate()
