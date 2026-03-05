ae2d44687f4ee1cddacdb0759ce97c8cc2a2b0c0
NikShep21 <klimenko.leva.lol2@yandex.ru>
2026-03-05 12:58:30 +0800

    add new function multyplication with bug

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,11 +1,18 @@
 import sys
 
-from math_demo import add
+from math_demo import add, add_with_bug
 
 def test_addition():
-    assert 2 + 2 == 4
+    assert add(2, 2) == 4
+    assert add(3, 3) == 6
     print("Addition test passed.")
-
+    
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(3, 3) == 6
+    print("Addition with bug test passed.")
+    
 
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
