90702805a3acfc370cf1514700e41e7972635750
NikShep21 <klimenko.leva.lol2@yandex.ru>
2026-03-05 13:10:48 +0800

    show do-not-write-exhaustive-tests principles

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -16,6 +16,15 @@ def test_addition_duplicate():
     assert add(6, 7) == 6 + 7
     print("Addition duplicate test passed.")
 
+def test_addition_overkill():
+    for i in range(0, 2**32):
+        for j in range(0, 2**32):
+            assert add(i, j) == i + j
+            assert add(-i, -j) == -i + -j
+            assert add(i, -j) == i + -j
+            assert add(-i, j) == -i + j
+    print("Addition overkill test passed.")
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
