e45a318dab368357eec2de0ecfa8b6d8be33a71c
NikShep21 <klimenko.leva.lol2@yandex.ru>
2026-04-02 14:57:40 +0800

    add extentions

--- docs_domain/docs/source/conf.py
+++ docs_domain/docs/source/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "src"))
 
 project = 'sales'
 copyright = '2026, Lev'
@@ -15,7 +19,11 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-  "myst_parser"
+  "myst_parser",
+  "sphinx.ext.autodoc",
+  "sphinx.ext.napoleon",
+  "sphinx.ext.autosummary",
+  "sphinx.ext.viewcode"
 ]
 
 source_suffix = {
